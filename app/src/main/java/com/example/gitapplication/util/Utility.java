aaed5b56b7f54ca3456b24e03cf7d179d0f6f1fd
wy <707072781@qq.com>
2020-11-27 11:48:38 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/gitapplication/util/Utility.java
+++ app/src/main/java/com/example/gitapplication/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.gitapplication.db.City;
 import com.example.gitapplication.db.County;
 import com.example.gitapplication.db.Province;
+import com.example.gitapplication.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -69,6 +71,18 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
